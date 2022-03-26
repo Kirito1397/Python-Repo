230cebbb8b3f767f76ba9e30a7f38422b95d7efe
Kirito <siddharthsinghnirwan@gmail.com>
2022-03-27 00:03:51 +0530

    Defining Device Types for 'Seed File Generation.py'

--- Projects/Simple Scripts/Seed File Generation.py
+++ Projects/Simple Scripts/Seed File Generation.py
@@ -1,33 +1,52 @@
 def seed_generation(content):
     START_DEVICE,START_DEVICE_INTERFACE,END_DEVICE,END_DEVICE_INTERFACE,CONNECTOR_TYPE = content
-    wiring_template = (f"""<Seed>
+    
+    # Below function fetches the Device Type of a Device.
+    def  identify_device_type(device):
+        device_types = {'ier': 'InternetEdgeRouter',
+                        'eza': 'EdgeZoneAggregator',
+                        'ibr': 'InternetBackboneRouter',
+                        'icr': 'InternetCoreRouter',
+                        'rwa': 'RegionalAggregatorRouter',
+                        'car': 'CoreAggregatorRouter',
+                        'ear': 'EdgeAggregatorRouter',
+                        'owr': 'OneWanRouter'}
+        return  device_types.get(device[:3].lower())
+
+    # Provides with a Single wiring template per link
+    wiring_template = (f"""
                <Wiring>
-                   <Start DeviceType="EdgeZoneAggregator" StartDevice="{START_DEVICE}" ItfNames="{START_DEVICE_INTERFACE}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                   <End DeviceType="InternetEdgeRouter" EndDevice="{END_DEVICE}" ItfNames="{END_DEVICE_INTERFACE}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-               </Wiring>
-          </Seed>""")
+                   <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_DEVICE_INTERFACE}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                   <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_DEVICE_INTERFACE}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+               </Wiring>""")
     return wiring_template
 
 if __name__ == "__main__":
     ndt_links = []
     print("Kindly enter the link details: \n")
+    # Get all the links as input
     while True:
         per_line_input = list(map(str,input().split()))
-        # print(per_line_input)
         if per_line_input != []:
             ndt_links.append(per_line_input)
         else:
+            # Breaks the loop if an empty input is provided
             break
 
-    # print(ndt_links)
+    # Define an empty string and then later join all the wriring templates to it.
     seed_file = ""
+
+    # Run Loop to create and join wriring templates
     for link in ndt_links:
-        seed_file = "\n".join((seed_file,seed_generation(link)))
+        seed_file = "".join((seed_file,seed_generation(link)))
     print(seed_file)
 
+
+'''
 # TESTCASE:
 
 # ier01.atl31 et-0/0/1 eza01.ztl30 et-0/0/2 LR4
 # ier02.atl31 et-0/0/3 eza02.ztl30 et-0/0/4 LR4
 # ier03.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 LR4
 # ier04.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 LR4
+'''
