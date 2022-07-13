f1e62f5e041a4cfb811c6543fcddb3af884d9f3c
Kirito <siddharthsinghnirwan@gmail.com>
2022-07-14 00:54:55 +0530

    Seed_File_generation Update and Refractor

--- Projects/Simple Scripts/Seed_File_Generator.py
+++ Projects/Simple Scripts/Seed_File_Generator.py
@@ -1,5 +1,20 @@
-# Below function fetches the Device Type of a Device.
+import re
+
+
+def user_prompt():
+    print("""Kindly select the desired option from below:\n\t1. New Build NDT\n\t2. Migration NDT\n""")
+    main_menu_response = int(input())
+    if main_menu_response == 1:
+        return ("\t\tThe Feature is not yet ready!\t\t")
+    else:
+        print("""Kindly confirm the type of Migration NDT from below options:\n\t1. Refresh\n\t2. Link Migration""")
+        migration_menu_response = int(input())
+        return seed_generation(migration_menu_response)
+
+
 def identify_device_type(device):
+    '''Function fetches the Device Type of a Device.'''
+
     device_types = {'ier': 'InternetEdgeRouter',
                     'eza': 'EdgeZoneAggregator',
                     'ibr': 'InternetBackboneRouter',
@@ -8,86 +23,106 @@ def identify_device_type(device):
                     'car': 'CoreAggregatorRouter',
                     'ear': 'EdgeAggregatorRouter',
                     'ter': 'TransitEdgeRouter',
-                    'owr': 'OneWanRouter'}
-    return device_types.get(device[:3].lower())
-
-def seed_generation(content):
-
-    # Gets the number of items provided as input per link
-    items = len(content)
-
-    if items < 6:
-        START_DEVICE, START_PORT, END_DEVICE, END_PORT, CONNECTOR_TYPE = content
-
-        # Provides with a Single wiring template per link
-        wiring_template = (f"""
-                   <Wiring>
-                       <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                       <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                       <LinkCount="1" LinkState="Production" />
-                   </Wiring>""")
-        return wiring_template
-
-    elif items == 6:
-        START_DEVICE, START_PORT, END_DEVICE, END_PORT, CONNECTOR_TYPE, SRLG_ID = content
-
-        # Provides with a Single wiring template per link
-        wiring_template = (f"""
-                   <Wiring>
-                       <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                       <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                       <LinkCount="1" LinkState="Production" SrlgId="{SRLG_ID}" />
-                   </Wiring>""")
-        return wiring_template
-
-    elif items == 10:
-        START_DEVICE, START_PORT, END_DEVICE, END_PORT, CONNECTOR_TYPE, SRLG_ID, START_IPV4, START_IPV46, END_IPV4, END_IPV6 = content
-
-        # Provides with a Single wiring template per link
-        wiring_template = (f"""
-                   <Wiring>
-                       <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                       <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                       <LinkCount="1" LinkState="Production" SrlgId="{SRLG_ID}" />
-                   </Wiring>""")
-        return wiring_template
+                    'owr': 'OneWanRouter',
+                    'sw': 'SwanRouter',
+                    '96cbe': 'CoreRouter'}
+    if bool(re.findall("^\S+-96cbe-\d\w$", device)):
+        return device_types.get('96cbe')
+    elif bool(re.findall("^\S+-\d+-\d+-\d+sw$", device)):
+        return device_types.get('sw')
+    else:
+        return device_types.get(device[:3].lower())
 
 
-if __name__ == "__main__":
-    
+def link_input():
     ndt_links = []
-    print("Kindly enter the link details: \n")
-    # Get all the links as input
     while True:
         per_line_input = list(map(str, input().split()))
-        if per_line_input != []:
+        if bool(per_line_input):
             ndt_links.append(per_line_input)
         else:
             # Breaks the loop if an empty input is provided
-            break
+            return ndt_links
+
+
+def seed_generation(option):
+
+    print('Provide the input links:')
+
+    def process_link(arr, func):
+        seed_file = "<Seed>"
+
+        # Run Loop to create and join wriring templates
+        for link in arr:
+            seed_file = "".join((seed_file, func(link)))
+        print(seed_file)
+
+    def refresh_ndt(content):
+
+        # Gets the number of items provided as input per link
+        items = len(content)
+
+        if items < 6:
+            START_DEVICE, START_PORT, END_DEVICE, END_PORT, CONNECTOR_TYPE = content
+
+            # Provides with a Single wiring template per link
+            wiring_template = (f"""
+                    <Wiring>
+                        <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                        <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                        <LinkCount="1" LinkState="Migration" />
+                    </Wiring>""")
+            return wiring_template
+
+        elif items == 6:
+            START_DEVICE, START_PORT, END_DEVICE, END_PORT, CONNECTOR_TYPE, SRLG_ID = content
+
+            # Provides with a Single wiring template per link
+            wiring_template = (f"""
+                    <Wiring>
+                        <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                        <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" />
+                    </Wiring>""")
+            return wiring_template
+
+        elif items == 10:
+            START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL, CDBID, CONNECTOR_TYPE = content
+
+            # Provides with a Single wiring template per link
+            wiring_template = (f"""
+                    <Wiring>
+                        <Start DeviceType="{identify_device_type(START_DEVICE)}" StartDevice="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                        <End DeviceType="{identify_device_type(END_DEVICE)}" EndDevice="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
+                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" />
+                    </Wiring>""")
+            return wiring_template
+
+    if option == 1:
+        process_link(link_input(), refresh_ndt)
+    else:
+        return 'Feature not Ready!'
+
+
+if __name__ == "__main__":
+
+    user_prompt()
 
     # Input the desired File Name for the seed.
     seed_file_name = str(input("Provide the desired file name for the Seed: "))
 
     # Define an string with <Seed> tag and then later join all the wriring templates to it.
-    seed_file = "<Seed>"
-
-    # Run Loop to create and join wriring templates
-    for link in ndt_links:
-        seed_file = "".join((seed_file, seed_generation(link)))
-    print(seed_file)
 
     # Define a variable conatining system location of the Seed to be generated.
     FILE_PATH = "C:/Users/Kirito/Desktop/"+seed_file_name+".xml"
 
     # Write the contents of the Seed file, and add the closing </Seed> tag with it.
-    try:
-        with open(FILE_PATH, 'x') as file:
-            file.writelines(seed_file + '\n</Seed>')
-    except OSError as e:
-        print(e,"\nKindly provide another seed name.")
-        exit()
-
+    # try:
+    #     with open(FILE_PATH, 'x') as file:
+    #         file.writelines(seed_file + '\n</Seed>')
+    # except OSError as e:
+    #     print(e,"\nKindly provide another seed name.")
+    #     exit()
 
 
 '''
