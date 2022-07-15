7e1b3722b52d77cb16783ddbae62c23d69ad18be
Kirito <siddharthsinghnirwan@gmail.com>
2022-07-15 14:12:42 +0530

    Define re_ip function and refractor code

--- Projects/Simple Scripts/Seed_File_Generator.py
+++ Projects/Simple Scripts/Seed_File_Generator.py
@@ -2,10 +2,14 @@ import re
 
 
 def user_prompt():
-    print("""Kindly select the desired option from below:\n\t1. New Build NDT\n\t2. Migration NDT\n""")
+    '''Function to process user choices in Menu'''
+
+    print("""Kindly select the desired option from below:\n\t1. New Build NDT\n\t2. Migration NDT\n\t3. Re-IP NDT\n""")
     main_menu_response = int(input())
     if main_menu_response == 1:
         return ("\t\tThe Feature is not yet ready!\t\t")
+    elif main_menu_response == 3:
+        return seed_generation(3)
     else:
         print("""Kindly confirm the type of Migration NDT from below options:\n\t1. Refresh\n\t2. Link Migration""")
         migration_menu_response = int(input())
@@ -35,6 +39,8 @@ def identify_device_type(device):
 
 
 def link_input():
+    '''Function used to store all the provided links in a list'''
+
     ndt_links = []
     while True:
         per_line_input = list(map(str, input().split()))
@@ -47,8 +53,6 @@ def link_input():
 
 def seed_generation(option):
 
-    print('Provide the input links:')
-
     def process_link(arr, func):
         seed_file = "<Seed>"
 
@@ -60,58 +64,6 @@ def seed_generation(option):
     def refresh_ndt(content):
 
         # Gets the number of items provided as input per link
-        items = len(content)
-
-        if items < 6:
-            START_DEVICE, START_PORT, END_DEVICE, END_PORT, CONNECTOR_TYPE = content
-
-            # Provides with a Single wiring template per link
-            wiring_template = (f"""
-                    <Wiring>
-                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                        <LinkCount="1" LinkState="Migration" />
-                    </Wiring>""")
-            return wiring_template
-
-        elif items == 6:
-            START_DEVICE, START_PORT, END_DEVICE, END_PORT, SRLG_ID, CONNECTOR_TYPE = content
-
-            # Provides with a Single wiring template per link
-            wiring_template = (f"""
-                    <Wiring>
-                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" />
-                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" />
-                    </Wiring>""")
-            return wiring_template
-
-        elif items == 10:
-            START_DEVICE, START_PORT, END_DEVICE, END_PORT, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, CONNECTOR_TYPE = content
-
-            # Provides with a Single wiring template per link
-            wiring_template = (f"""
-                    <Wiring>
-                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" IPv4="{START_IPV4}" IPv6="{START_IPV6}"/>
-                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" NewPortChannel="True" IPv4="{END_IPV4}" IPv6="{END_IPV6}"/>
-                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" />
-                    </Wiring>""")
-            return wiring_template
-        
-        elif items == 11:
-            START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, START_IPV4, START_IPV6, END_IPV4, END_IPV6, CDBID, CONNECTOR_TYPE  = content
-
-            # Provides with a Single wiring template per link
-            wiring_template = (f"""
-                    <Wiring>
-                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}" IPv6="{START_IPV6}" />
-                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}" IPv6="{END_IPV6}" />
-                        <LinkCount="1" LinkState="Migration" />
-                        <Property Name="CDBID">{CDBID}</Property>
-                    </Wiring>""")
-            return wiring_template
-
-        elif items == 13:
             START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CONNECTOR_TYPE = content
 
             # Provides with a Single wiring template per link
@@ -119,26 +71,29 @@ def seed_generation(option):
                     <Wiring>
                         <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}" IPv6="{START_IPV6}" />
                         <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}" IPv6="{END_IPV6}" />
-                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" SolutionId="{SOLUTION_ID}" Channel="{CHANNEL_NUM}" OpticalChannelType="Line" />
+                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" RefreshOpticalOverlay="True" />
                     </Wiring>""")
             return wiring_template
 
-        elif items == 14:
+    def re_ip(content):
+
             START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM , CDBID, CONNECTOR_TYPE = content
 
-            # Provides with a Single wiring template per link
             wiring_template = (f"""
                     <Wiring>
                         <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}" IPv6="{START_IPV6}" />
                         <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}" IPv6="{END_IPV6}" />
-                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" SolutionId="{SOLUTION_ID}" Channel="{CHANNEL_NUM}" OpticalChannelType="Line" />
+                        <LinkCount="1" LinkState="Production" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" SolutionId="{SOLUTION_ID}" Channel="{CHANNEL_NUM}" OpticalChannelType="Line" />
                         <Property Name="CDBID">{CDBID}</Property>
                     </Wiring>""")
             return wiring_template
-        else:
-            return f'\nProvide Current number of fields. One of the field is missing.'
+
     if option == 1:
+        print("Provide links as input in below format:\n\n\tSTART_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CONNECTOR_TYPE\n")
         process_link(link_input(), refresh_ndt)
+    elif option == 1:
+        print("Provide links as input in below format:\n\n\tSTART_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CDBID, CONNECTOR_TYPE\n")
+        process_link(link_input(), re_ip)
     else:
         return 'Feature not Ready!'
 
@@ -148,12 +103,12 @@ if __name__ == "__main__":
     user_prompt()
 
     # Input the desired File Name for the seed.
-    seed_file_name = str(input("Provide the desired file name for the Seed: "))
+    # seed_file_name = str(input("Provide the desired file name for the Seed: "))
 
     # Define an string with <Seed> tag and then later join all the wriring templates to it.
 
     # Define a variable conatining system location of the Seed to be generated.
-    FILE_PATH = "C:/Users/Kirito/Desktop/"+seed_file_name+".xml"
+    # FILE_PATH = "C:/Users/Kirito/Desktop/"+seed_file_name+".xml"
 
     # Write the contents of the Seed file, and add the closing </Seed> tag with it.
     # try:
@@ -165,10 +120,18 @@ if __name__ == "__main__":
 
 
 '''
-# TESTCASE:
+# TESTCASE 1:
 
 # ier01.atl31 et-0/0/1 eza01.ztl30 et-0/0/2 LR4
 # ier02.atl31 et-0/0/3 eza02.ztl30 et-0/0/4 LR4
 # ier03.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 LR4
 # ier04.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 LR4
+
+# TESTCASE 2:
+
+# ier01.atl31 et-0/0/1 eza01.ztl30 et-0/0/2 21 100235 10.10.10.1/31 201::0/126 10.10.10.2/31 201::1/126 THN02 20 LR4
+# ier02.atl31 et-0/0/3 eza02.ztl30 et-0/0/4 21 100235 10.10.11.1/31 202::0/126 10.10.11.2/31 202::1/126 THN02 21 LR4
+# ier03.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 21 100235 10.10.12.1/31 203::0/126 10.10.12.2/31 203::1/126 THN02 22 LR4
+# ier04.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 21 100235 10.10.13.1/31 204::0/126 10.10.13.2/31 204::1/126 THN02 23 LR4
+
 '''
