2feadca6169b26f913ebc2682eb4a1b951fc155e
Siddharth Nirwan <v-snirwa@microsoft.com>
2022-07-19 16:40:54 +0530

    Code Fix

--- Projects/Simple Scripts/Seed_File_Generator.py
+++ Projects/Simple Scripts/Seed_File_Generator.py
@@ -37,7 +37,8 @@ def identify_device_type(device):
                     '96cbe': 'CoreRouter'}
     if bool(re.findall("^\S+-96cbe-\d\w$", device)):
         return device_types.get('96cbe')
-    elif bool(re.findall("^\S+-\d+-\d+-\d+sw$", device)):
+    elif bool(re.findall("(?i)^\S+-\d+-\d+-\d+sw$", device)):
+        # (?i) means case Insesitive
         return device_types.get('sw')
     else:
         return device_types.get(device[:3].lower())
@@ -69,35 +70,54 @@ def seed_generation(option):
         print(seed_file)
 
     def refresh_ndt(content):
+        items = len(content)
 
-        # Gets the number of items provided as input per link
-            START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CONNECTOR_TYPE = content
+        if items == 10:
+            # Gets the number of items provided as input per link
+            START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, START_IPV4, START_IPV6, END_IPV4, END_IPV6, CONNECTOR_TYPE = content
 
             # Provides with a Single wiring template per link
             wiring_template = (f"""
                     <Wiring>
-                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}" IPv6="{START_IPV6}" />
-                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}" IPv6="{END_IPV6}" />
-                        <LinkCount="1" LinkState="Migration" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" RefreshOpticalOverlay="True" />
+                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="^{START_DEVICE}$" Scope="Datacenter" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}/31" IPv6="{START_IPV6}/126" />
+                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="^{END_DEVICE}$" Scope="Datacenter" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}/31" IPv6="{END_IPV6}/126" />
+                        <LinkCount="1" LinkState="Migration" LinkCountScope="Local" />
                     </Wiring>""")
             return wiring_template
 
-    def re_ip(content):
+        elif items == 11:
+            # Gets the number of items provided as input per link
+            START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, CONNECTOR_TYPE = content
 
-            START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM , CDBID, CONNECTOR_TYPE = content
-
+            # Provides with a Single wiring template per link
             wiring_template = (f"""
                     <Wiring>
-                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="{START_DEVICE}" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}" IPv6="{START_IPV6}" />
-                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="{END_DEVICE}" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}" IPv6="{END_IPV6}" />
-                        <LinkCount="1" LinkState="Production" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" SolutionId="{SOLUTION_ID}" Channel="{CHANNEL_NUM}" OpticalChannelType="Line" />
-                        <Property Name="CDBID">{CDBID}</Property>
+                        <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="^{START_DEVICE}$" Scope="Datacenter" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}/31" IPv6="{START_IPV6}/126" />
+                        <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="^{END_DEVICE}$" Scope="Datacenter" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}/31" IPv6="{END_IPV6}/126" />
+                        <LinkCount="1" LinkState="Migration" LinkCountScope="Local" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" RefreshOpticalOverlay="True" />
                     </Wiring>""")
             return wiring_template
+        else:
+            return 'One or more field is missing!'
+
+    def re_ip(content):
+        items = len(content)
+
+        START_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CDBID, CONNECTOR_TYPE = content
+
+        wiring_template = (f"""
+                <Wiring>
+                    <Start DeviceType="{identify_device_type(START_DEVICE)}" DeviceRegex="^{START_DEVICE}$" Scope="Datacenter" ItfNames="{START_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{START_IPV4}/31" IPv6="{START_IPV6}/126" />
+                    <End DeviceType="{identify_device_type(END_DEVICE)}" DeviceRegex="^{END_DEVICE}$" Scope="Datacenter" ItfNames="{END_PORT}" ConnectorType="{CONNECTOR_TYPE}" PortChannel="{PORT_CHANNEL}" IPv4="{END_IPV4}/31" IPv6="{END_IPV6}/126" />
+                    <LinkCount="1" LinkState="Production" LinkCountScope="Local" SrlgId="{SRLG_ID}" AppendOpticalOverlay="true" SolutionId="{SOLUTION_ID}" Channel="{CHANNEL_NUM}" OpticalChannelType="Line" />
+                    <Property Name="CDBID">{CDBID}</Property>
+                </Wiring>""")
+        return wiring_template
 
     # Making Decision based on User Input
     if option == '2.1':
-        print("\nProvide links as input in below format:\n\n\tSTART_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CONNECTOR_TYPE\n")
+        # Refresh-NDT
+        print("\nProvide links as input in below format(if field is not present, just give blank):\n\n\tSTART_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, CONNECTOR_TYPE\n")
         process_links(link_input(), refresh_ndt)
     elif option == '1.3':
         print("\nProvide links as input in below format:\n\n\tSTART_DEVICE, START_PORT, END_DEVICE, END_PORT, PORT_CHANNEL, SRLG_ID, START_IPV4, START_IPV6, END_IPV4, END_IPV6, SOLUTION_ID, CHANNEL_NUM, CDBID, CONNECTOR_TYPE\n")
@@ -145,16 +165,16 @@ if __name__ == "__main__":
 '''
 # TESTCASE 1:
 
-# ier01.atl31 et-0/0/1 eza01.ztl30 et-0/0/2 LR4
-# ier02.atl31 et-0/0/3 eza02.ztl30 et-0/0/4 LR4
-# ier03.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 LR4
-# ier04.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 LR4
+# ier01.ztl91 et-0/0/1 eza01.ztl90 et-0/0/2 LR4
+# ier02.ztl91 et-0/0/3 eza02.ztl90 et-0/0/4 LR4
+# ier03.ztl91 et-0/0/3 eza04.ztl90 et-0/0/4 LR4
+# ier04.ztl91 et-0/0/3 eza04.ztl90 et-0/0/4 LR4
 
 # TESTCASE 2:
 
-# ier01.atl31 et-0/0/1 eza01.ztl30 et-0/0/2 21 100235 10.10.10.1/31 201::0/126 10.10.10.2/31 201::1/126 THN02 20 LR4
-# ier02.atl31 et-0/0/3 eza02.ztl30 et-0/0/4 21 100235 10.10.11.1/31 202::0/126 10.10.11.2/31 202::1/126 THN02 21 LR4
-# ier03.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 21 100235 10.10.12.1/31 203::0/126 10.10.12.2/31 203::1/126 THN02 22 LR4
-# ier04.atl31 et-0/0/3 eza04.ztl30 et-0/0/4 21 100235 10.10.13.1/31 204::0/126 10.10.13.2/31 204::1/126 THN02 23 LR4
+# ier01.ztl91 et-0/0/1 eza01.ztl90 et-0/0/2 21 100235 10.10.10.1/31 201::0/126 10.10.10.2/31 201::1/126 THN02 20 LR4
+# ier02.ztl91 et-0/0/3 eza02.ztl90 et-0/0/4 21 100235 10.10.11.1/31 202::0/126 10.10.11.2/31 202::1/126 THN02 21 LR4
+# ier03.ztl91 et-0/0/3 eza04.ztl90 et-0/0/4 21 100235 10.10.12.1/31 203::0/126 10.10.12.2/31 203::1/126 THN02 22 LR4
+# ier04.ztl91 et-0/0/3 eza04.ztl90 et-0/0/4 21 100235 10.10.13.1/31 204::0/126 10.10.13.2/31 204::1/126 THN02 23 LR4
 
 '''
